cec48b7773644499eab8d1acd0d92e8bb49b7e31
bmng-dev <bmng-dev@users.noreply.github.com>
2016-08-30 13:49:57 +0000

    Quote path

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
-C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat x86
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 CL /I C:\OpenSSL-Win32\include /INCREMENTAL bmpow.c /MT /link /DLL /OUT:bmpow32.dll /LIBPATH:"C:\OpenSSL-Win32\lib" libeay32.lib ws2_32.lib
