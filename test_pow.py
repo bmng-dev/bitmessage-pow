3e020f4cdbc08dcc73d69f1c61973768a6f235ed
bmng-dev <bmng-dev@users.noreply.github.com>
2016-09-05 08:22:11 +0000

    test_pow.py improvements

--- test_pow.py
+++ test_pow.py
@@ -1,15 +1,27 @@
+import os
+import sys
+
+platform = os.environ.get('PLATFORM', '').lower()
+interpreter_bits = (sys.maxsize.bit_length() + 7) / 8 * 8
+
+if platform == 'x64' and interpreter_bits != 64:
+   raise SystemExit
+
+if platform in ['win32', 'x86'] and interpreter_bits != 32:
+   raise SystemExit
+
+
 import ctypes
 import hashlib
 import logging
-import os
 import struct
-import sys
 import timeit
 
 logging.basicConfig(format='%(levelname)8s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-Q = struct.Struct('!Q')
+Q_BE = struct.Struct('>Q')
+Q_LE = struct.Struct('<Q')
 
 payload = '\x00' * 4
 time_to_live = 300
@@ -20,58 +32,42 @@ payload = payload[:2**18]
 adjustedLength = len(payload) + padding
 digest = hashlib.sha512(payload).digest()
 target = 2 ** 64 / (trials * (adjustedLength + ((time_to_live * adjustedLength) / (2 ** 16))))
+target_bytes = Q_BE.pack(target)
 
-def do_pow_py():
-    target_bytes = Q.pack(target)
+def do_pow_py_be():
     message = bytearray(8 + len(digest))
     message[8:] = digest
     for nonce in xrange(0x7FFFFFFF):
-        Q.pack_into(message, 0, nonce)
+        Q_BE.pack_into(message, 0, nonce)
         if 0 >= cmp(hashlib.sha512(hashlib.sha512(message).digest()).digest(), target_bytes):
             return nonce
 
-def do_pow_x86():
-    lib = ctypes.CDLL('bin\\bmpow32.dll')
+def do_pow_py_le():
+    message = bytearray(8 + len(digest))
+    message[8:] = digest
+    for nonce in xrange(0x7FFFFFFF):
+        Q_LE.pack_into(message, 0, nonce)
+        if 0 >= cmp(hashlib.sha512(hashlib.sha512(message).digest()).digest(), target_bytes):
+            return nonce
+
+def do_pow_vs():
+    lib = ctypes.CDLL('bin\\bmpow{0}.dll'.format(interpreter_bits))
 
     lib.BitmessagePOW.argtypes = [ctypes.c_char_p, ctypes.c_uint64]
     lib.BitmessagePOW.restype = ctypes.c_uint64
 
     return lib.BitmessagePOW(digest, target)
 
-def do_pow_x64():
-    lib = ctypes.CDLL('bin\\bmpow64.dll')
+def do_pow_mingw():
+    lib = ctypes.CDLL('bmpow{0}_m.dll'.foramt(interpreter_bits))
 
     lib.BitmessagePOW.argtypes = [ctypes.c_char_p, ctypes.c_uint64]
     lib.BitmessagePOW.restype = ctypes.c_uint64
 
     return lib.BitmessagePOW(digest, target)
 
-def do_pow_x86_m():
-    lib = ctypes.CDLL('bmpow32_m.dll')
-
-    lib.BitmessagePOW.argtypes = [ctypes.c_char_p, ctypes.c_uint64]
-    lib.BitmessagePOW.restype = ctypes.c_uint64
-
-    return lib.BitmessagePOW(digest, target)
-
-def do_pow_x64_m():
-    lib = ctypes.CDLL('bmpow64_m.dll')
-
-    lib.BitmessagePOW.argtypes = [ctypes.c_char_p, ctypes.c_uint64]
-    lib.BitmessagePOW.restype = ctypes.c_uint64
-
-    return lib.BitmessagePOW(digest, target)
-
-def do_pow_x86_alt():
-    lib = ctypes.CDLL('bitmsghash32.dll')
-
-    lib.BitmessagePOW.argtypes = [ctypes.c_char_p, ctypes.c_uint64]
-    lib.BitmessagePOW.restype = ctypes.c_uint64
-
-    return lib.BitmessagePOW(digest, target)
-
-def do_pow_x64_alt():
-    lib = ctypes.CDLL('bitmsghash64.dll')
+def do_pow_alt():
+    lib = ctypes.CDLL('bitmsghash{0}.dll'.format(interpreter_bits))
 
     lib.BitmessagePOW.argtypes = [ctypes.c_char_p, ctypes.c_uint64]
     lib.BitmessagePOW.restype = ctypes.c_uint64
@@ -79,13 +75,7 @@ def do_pow_x64_alt():
     return lib.BitmessagePOW(digest, target)
 
 
-pow_funcs = [do_pow_py]
-if sys.maxsize > 0xFFFFFFFF:
-    pow_funcs.extend([do_pow_x64, do_pow_x64_m, do_pow_x64_alt])
-else:
-    pow_funcs.extend([do_pow_x86, do_pow_x86_m, do_pow_x86_alt])
-
-for do_pow in pow_funcs:
+for do_pow in [do_pow_py_be, do_pow_py_le, do_pow_vs, do_pow_mingw, do_pow_alt]:
     try:
         start = timeit.default_timer()
         nonce = do_pow()
